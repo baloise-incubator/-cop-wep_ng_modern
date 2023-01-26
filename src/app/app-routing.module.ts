364ae2645dc64451bdabc2c7d3a90838f1baef04
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-01-26 14:45:09 +0100

    add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FirstPageComponent } from './pages/first-page/first-page.component';
-import { SecondPageComponent } from './pages/second-page/second-page.component';
 
-const routes: Routes = [
-  { path: 'first-component', component: FirstPageComponent },
-  { path: 'second-component', component: SecondPageComponent },
+export const routes: Routes = [
+  {
+    path: 'first-component',
+    loadChildren: () =>
+      import('./pages/first-page/first-page.module').then(
+        (m) => m.FirstPageModule
+      ),
+  },
+  {
+    path: 'second-component',
+    loadChildren: () =>
+      import('./pages/second-page/second-page.module').then(
+        (m) => m.SecondPageModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'first-component',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'first-component',
+  },
 ];
 
 @NgModule({
--- src/app/pages/second-page/second-page.module.ts
+++ src/app/pages/second-page/second-page.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SecondPageComponent } from './second-page.component';
 
 @NgModule({
   declarations: [SecondPageComponent],
-  exports: [SecondPageComponent],
-  imports: [SharedModule],
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: SecondPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'first-component',
+        pathMatch: 'full',
+      },
+    ]),
+  ],
 })
 export class SecondPageModule {}
